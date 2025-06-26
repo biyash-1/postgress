2deda03f9ce73b77c78aebbb066aa9b4a593b278
biyash-1 <sthaalish39@gmail.com>
2025-06-26 20:42:49 +0545

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import { Button } from './ui/button'
-import Link from 'next/link'
-const Navbar = () => {
+import { SignedIn, SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
+import { syncUser } from '@/app/actions/useraction'
+
+export async function Navbar() {
+  await syncUser();
   return (
-    <div className='p-2 bg-cyan-200 '>
-      <nav className='flex  items-center justify-around'>
+    <div className='p-2 bg-cyan-200'>
+      <nav className='flex items-center justify-around'>
         <div>
-            <h1 className='text-red-400'>Task@</h1>
+          <h1 className='text-red-400'>Task@</h1>
         </div>
         <div>
-            <ul className='flex gap-2 items-center   '>
-                <li className='className="bg-blue-600 hover:text-blue-700 font-semibold py-2 px-4 rounded transition duration-200"
-  '>Home</li>
-                <li>About</li>
-                <li>My tasks</li>
-                <li>completed</li>
-            </ul>
+          <ul className='flex gap-2 items-center'>
+            <li className='bg-blue-600 hover:text-blue-700 font-semibold py-2 px-4 rounded transition duration-200'>Home</li>
+            <li>About</li>
+            <li>My tasks</li>
+            <li>completed</li>
+          </ul>
         </div>
         <div>
-          <Link href ="/login">
-           <Button>Login</Button>
-          </Link>
+          <SignedIn >
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode='modal' forceRedirectUrl="/dashboard"  >
+              <Button>Login</Button>
+            </SignInButton>
+          </SignedOut>
         </div>
       </nav>
     </div>
