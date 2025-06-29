78dedf8036913cd77601da9a11e97df5e1b92c36
biyash-1 <sthaalish39@gmail.com>
2025-06-29 23:32:15 +0545

    eoute

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,65 +1,111 @@
 import { NextRequest,NextResponse } from "next/server";
 import { prisma } from "@/app/lib/prisma";
-import jwt from 'jsonwebtoken'
-import { cookies } from "next/headers";
+import {auth} from  "@clerk/nextjs/server"
+// import jwt from 'jsonwebtoken'
+// import { cookies } from "next/headers";
 
-const JWT_SECRET = process.env.JWT_SECRET;
 
-export async function POST(req:NextRequest){
+// const JWT_SECRET = process.env.JWT_SECRET;
 
-const body = await req.json();
-const {task} = body;
 
-if(!task)
-{
-    return NextResponse.json({message:"tasks cant be empty"},{status:400});
+// for clerk based authentication
 
-}
+export  async function POST(req:Request) {
 
-const  cookieStore = await cookies();
-const token =  cookieStore.get("token")?.value;
+  try{
+    const {userId} = await auth();
+    if(!userId) {
+      return NextResponse.json({"message":"unauthorized"})
+    }
 
-if(!token){
-    return NextResponse.json({message:"task cant be empty"},{status:401});
-}
-  const decoded = jwt.verify(token, JWT_SECRET!) as { id: number };
+    const body = await req.json();
+    const {task} = body;
+      if (!task) {
+    return NextResponse.json({ message: "Task can't be empty" }, { status: 400 });
+  }
+
+  const dbUser = await prisma.user.findUnique({
+    where:{
+      clerkId:userId
+    }
+
+  });
+  if(!dbUser){
+    return NextResponse.json({message:"user not found"})
+  }
   const newTask =  await prisma.tasks.create({
     data:{
-        task,
-        userId:decoded.id
+      task,
+      userId:dbUser.id,
     }
+    
   })
-  return NextResponse.json({message:"task created sucessfully",task:newTask
+    return NextResponse.json({ message: "Task created", task: newTask });
   }
-  )
-}
-
-
-export async function GET(req:Request){
-  try{
- const cookieStore = await cookies();
- const token = cookieStore.get("token")?.value;
-
- if(!token){
-    return NextResponse.json({messag:"no token provided"});
- }
- const decoded = jwt.verify(token,JWT_SECRET!) as {id:number}
- const tasks = await prisma.tasks.findMany({
-  where:{userId:decoded.id},
-  orderBy:{createdAt:"desc"}
-
- })
- return NextResponse.json({tasks})
-
-
+  
+  catch(error){
+    console.log(error)
 
   }
-  catch(error)
-
-{
-    return NextResponse.json({message:"error fetching data"},{status:500})
-
 }
 
-}
+// token based system
 
+
+// export default async function GET(req:Request){
+//   try{
+//     const cookieStore = await cookies();
+//     const token = cookieStore.get("token")?.value;
+    
+//     if(!token){
+//       return NextResponse.json({messag:"no token provided"});
+//     }
+//     const decoded = jwt.verify(token,JWT_SECRET!) as {id:number}
+//     const tasks = await prisma.tasks.findMany({
+//       where:{userId:decoded.id},
+//       orderBy:{createdAt:"desc"}
+      
+//     })
+//     return NextResponse.json({tasks})
+    
+    
+
+//   }
+//   catch(error)
+  
+//   {
+//     return NextResponse.json({message:"error fetching data"},{status:500})
+    
+//   }
+  
+// }
+
+
+// export async function POST(req:NextRequest){
+
+// const body = await req.json();
+// const {task} = body;
+
+// if(!task)
+// {
+//     return NextResponse.json({message:"tasks cant be empty"},{status:400});
+
+// }
+
+// const  cookieStore = await cookies();
+// const token =  cookieStore.get("token")?.value;
+
+// if(!token){
+//     return NextResponse.json({message:"task cant be empty"},{status:401});
+// }
+//   const decoded = jwt.verify(token, JWT_SECRET!) as { id: number };
+//   const newTask =  await prisma.tasks.create({
+//     data:{
+//         task,
+//         userId:decoded.id
+//     }
+//   })
+//   return NextResponse.json({message:"task created sucessfully",task:newTask
+//   }
+//   )
+// }
